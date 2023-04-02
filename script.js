661369935e4367bae4dca4970955041d3c2fd5fb
E.J. Perez <ejperez37@gmail.com>
2023-04-02 11:58:39 -0400

    Change if else block to switch statement and display clean code etiquette

--- script.js
+++ script.js
@@ -1,52 +1,84 @@
 function getComputerSelection() {
-    // Step 1: Create an array of choices
+    // Create an array of choices
     const choices = ['rock', 'paper', 'scissors'];
   
-    // Step 2: Generate a random number between 0 and 2 (inclusive)
+    // Generate a random number between 0 and 2 (inclusive)
     const randomIndex = Math.floor(Math.random() * choices.length);
   
-    // Step 3: Use the random number to pick an element from the array
+    // Use the random number to pick an element from the array
     const computerSelection = choices[randomIndex];
   
-    // Step 4: Return the selection
+    // Return the selection
     return computerSelection;
   }
 
 function playRound(playerSelection, computerSelection) {
-    // Step 1: Get playerSelection via prompt
+    // Get playerSelection via prompt
     playerSelection = prompt("pick your fighter! rock, paper, scissors, SHOOT!", "you gotta shoot man");
-    // Step 2: Make sure playerSelection is lowercase
+    // Make sure playerSelection is lowercase
     playerSelection = playerSelection.toLowerCase();
-    // Step 3: Get computerSelection via getComputerSelection()
+    // Get computerSelection via getComputerSelection()
     computerSelection = getComputerSelection();
-    // Step 4: Compare playerSelection and computerSelection
-    if (playerSelection === computerSelection) {
-        console.log("It's a tie!");
+    // Compare playerSelection and computerSelection to determine the winner
+    switch (playerSelection) {
+        case "rock":
+          if (computerSelection == "scissors") {
+            return("You win!");
+          } else if (computerSelection == "paper") {
+            return("You lose!");
+          } else {
+            return("It's a tie!");
+          }
+        case "paper":
+          if (computerSelection == "rock") {
+            return("You win!");
+          } else if (computerSelection == "scissors") {
+            return("You lose!");
+          } else {
+            return("It's a tie!");
+          }
+        case "scissors":
+          if (computerSelection == "paper") {
+            return("You win!");
+          } else if (computerSelection == "rock") {
+            return("You lose!");
+          } else {
+            return("It's a tie!");
+          }
+        default:
+          return("That's not a valid choice!");
+      }
+}
+  // Create a function called game() that plays five rounds and keeps score
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;  
+  // Create a loop that plays five rounds
+    for (let i = 0; i < 5; i++) {
+       switch (playRound()) {
+        case "You win!":
+          ++playerScore;
+          console.log("You win! Player: " + playerScore + " Computer: " + computerScore);
+          break;
+        case "You lose!":
+          ++computerScore;
+          console.log("You lose! Player: " + playerScore + " Computer: " + computerScore);
+          break;
+        case "It's a tie!":
+          console.log("You tied! No points awarded. Player: " + playerScore + " Computer: " + computerScore);
+          break;
+        default:
+          console.log("That's not a valid choice! Try again.");
+}
     }
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("You win! Rock beats Scissors!");
+// After the loop ends, log the final scores and declare the winner
+    if (playerScore > computerScore) {
+      console.log("You win the game!");
     }
-    else if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log("You lose! Paper beats Rock!");
-    } 
-    else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("You win! Paper beats Rock!");
-    } 
-    else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log("You lose! Scissors beats Paper!");
+    else if (playerScore < computerScore) {
+      console.log("You lose the game!");
     }
-    else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("You win! Scissors beats Paper!");
+   else {
+      console.log("It's a tie!");
     }
-    else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log("You lose! Rock beats Scissors!");
-    }
-    else {
-        console.log("That's not a valid choice!");
-    }
-  }
-
-
-  
-  
-
+}
